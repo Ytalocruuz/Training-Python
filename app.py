4291a29ad13b08b1d497c9e410d391fca172bb3b
Ytalo Cruz <ytalocrz@gmail.com>
2023-04-13 23:05:36 -0300

    Treinando

--- app.py
+++
@@ -1,6 +0,0 @@
-#Primeiro codifo Python
-nome = 'Ytalo';
-print("Ola, seja bem vindo", nome);
-
-numero = 1;
-print(numero + 3);
---
+++ variavel.py
@@ -0,0 +1,19 @@
+pessoa = 'Ytalo Cruz'
+idade = 21
+altura = 1.64
+
+print(pessoa, idade, altura)
+
+poncetagem = 0.6
+numero = -100
+print(numero)
+
+minhaIdade = ""
+minhaIdade = 2001 - 2023
+print(minhaIdade)
+
+NOME = "TESTE1"
+nome = "Teste2"
+
+print(NOME, nome)
+
