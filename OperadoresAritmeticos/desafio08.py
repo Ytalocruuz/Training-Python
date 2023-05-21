c1056defbdf10049a205da4c653e9c0eaf2db24b
Ytalo Cruz <ytalocrz@gmail.com>
2023-05-20 23:07:09 -0400

    Desafios

--- OperadoresAritmeticos/desafio08.py
+++ OperadoresAritmeticos/desafio08.py
@@ -1,1 +1,8 @@
 #Escreva um progrma que leia um valor em metros e exiba convertido em centimetros e milimetros. 
+
+metros  = float(input('Digite um valor em metros: '))
+
+c = metros * 100
+m = metros * 1000
+
+print('Valor informado: {}, em centimetros {}, em milimetros: {}'.format(metros, c,m))
